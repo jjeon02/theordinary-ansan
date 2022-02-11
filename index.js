a141b418c9cf7d0721297dc192bf33e7b981772a
jjeon02 <june.jeon@gmail.com>
2022-02-11 09:16:21 +0900

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+// GOBACK BUTTON
+  function goBack() {
+      window.history.back();
+  }
+
+//NAVIGATION
+function openNav() {
+  document.getElementById("sideNav").style.width= "100%";
+}
+
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+}
+   
